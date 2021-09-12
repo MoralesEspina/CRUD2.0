d6abfc4911d9b34d47fa092240c384e13659e97e
MoralesEspina <MoralesEspina92@gmail.com>
2021-09-12 14:59:19 -0600

    make it better

--- configurations/db-conf.js
+++ configurations/db-conf.js
@@ -1,7 +1,7 @@
-    const host = "localhost";
-    const user = "root";
-    const password = 'admin';
-    const database = "estudiantes";
+    const host = "phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    const user = "o2pcqu2di101rbzr";
+    const password = 'pnzs8p59hrocvon8';
+    const database = "yt23oldwpm5owhkn";
 
     exports.host=host;
     exports.user=user;
